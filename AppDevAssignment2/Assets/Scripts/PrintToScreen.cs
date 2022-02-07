50035e9371370492eae83f6146b832a47202ea87
MurasakiMaho <xtremeversusgaming@gmail.com>
2022-02-07 22:30:08 +0800

    Added flee function to flee button
    50% chance to flee successfully
    Changing the scene to Gameplay

--- AppDevAssignment2/Assets/Scripts/PrintToScreen.cs
+++ AppDevAssignment2/Assets/Scripts/PrintToScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PrintToScreen : MonoBehaviour
 {
@@ -15,6 +16,13 @@ public class PrintToScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private IEnumerator Wait(float waitTime)
+    {
+        
+            yield return new WaitForSeconds(waitTime);
         
     }
 
@@ -36,5 +44,14 @@ public class PrintToScreen : MonoBehaviour
     public void ClickedButt4()
     {
         printout.text = "You tried running away!";
+        float RandomNumber = Random.Range(0, 100);
+        if (RandomNumber <= 50)
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+        else
+        {
+            printout.text = "You failed!";
+        }
     }
 }
