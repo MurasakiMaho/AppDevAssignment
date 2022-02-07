2a4d5fe9f223afcc634ea95918d327db401e0970
Trina-Rachelle-Tee <88866170+Trina-Rachelle-Tee@users.noreply.github.com>
2022-02-07 23:36:01 +0800

    working on a function that lets scenes transition

--- AppDevAssignment2/Assets/Scripts/soundReset.cs
+++ AppDevAssignment2/Assets/Scripts/soundReset.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class soundReset : MonoBehaviour
 {
     public Slider musicVol, SFXVol;
     public void resetValue()
