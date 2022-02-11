6a9fe4d3f366f4ae0b3bbeed44abf2e4076d4356
Trina-Rachelle-Tee <88866170+Trina-Rachelle-Tee@users.noreply.github.com>
2022-02-11 17:54:19 +0800

    JOYSTICK FINALLY WORKS PROPERLY!!!

--- AppDevAssignment2/Assets/Scripts/InventoryTab.cs
+++ AppDevAssignment2/Assets/Scripts/InventoryTab.cs
@@ -5,58 +5,24 @@ using UnityEngine;
 public class InventoryTab : MonoBehaviour
 {
     public GameObject tab1, tab2, tab3;
-    bool tab1on, tab2on, tab3on;
     // Start is called before the first frame update
-    void Start()
-    {
-        tabsOff();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (tab1on == true)
-            tab1.gameObject.SetActive(true);
-        else
-            tab1.gameObject.SetActive(false);
-        if (tab2on == true)
-            tab2.gameObject.SetActive(true);
-        else
-            tab2.gameObject.SetActive(false);
-        if (tab3on == true)
-            tab3.gameObject.SetActive(true);
-        else
-            tab3.gameObject.SetActive(false);
-    }
     public void activateTab1()
     {
 
-        tab1on = true;
-        tab2on = false;
-        tab3on = false;
+        tab1.gameObject.SetActive(true);
+        tab2.gameObject.SetActive(false);
+        tab3.gameObject.SetActive(false);
     }
     public void activateTab2()
     {
-        if (tab1 == true || tab2 == true || tab3 == true)
-        {
-            tab1on = false;
-            tab2on = true;
-            tab3on = false;
-        }
+        tab1.gameObject.SetActive(false);
+        tab2.gameObject.SetActive(true);
+        tab3.gameObject.SetActive(false);
     }
     public void activateTab3()
     {
-        if (tab1 == true || tab2 == true || tab3 == true)
-        {
-            tab1on = false;
-            tab2on = false;
-            tab3on = true;
-        }
-    }
-    public void tabsOff()
-    {
-           tab1on = false;
-           tab2on = false;
-           tab3on = false;
+        tab1.gameObject.SetActive(false);
+        tab2.gameObject.SetActive(false);
+        tab3.gameObject.SetActive(true);
     }
 }
--- AppDevAssignment2/Assets/Scripts/joystickMove.cs
+++ AppDevAssignment2/Assets/Scripts/joystickMove.cs
@@ -5,28 +5,26 @@ using UnityEngine;
 
 public class JoystickMove : MonoBehaviour
 {
-
-
-    public GameObject joystick;
-    public GameObject joystickBG;
-    public Vector2 joystickVec;
-    private Vector2 joystickTouchPos;
-    private Vector2 joystickOriginalPos;
+    public GameObject joystick;//joystick thumb
+    public GameObject joystickBG;//joystick background
+    public Vector2 joystickVec;//movement of the joystick to control player movement
+    private Vector2 joystickTouchPos;//location the player touches
+    private Vector2 joystickOriginalPos;//original position of joystick
     private float joystickRadius;
+    public Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
         joystickOriginalPos = joystickBG.transform.position;
-        joystickRadius = joystickBG.GetComponent<RectTransform>().sizeDelta.y / 4;
+        joystickRadius = joystickBG.GetComponent<RectTransform>().sizeDelta.y / 2;
     }
 
     public void PointerDown()
     {
-        joystick.transform.position = Input.mousePosition;
         joystickTouchPos = Input.mousePosition;
+        joystick.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
     }
-
     public void Drag(BaseEventData baseEventData)
     {
         PointerEventData pointerEventData = baseEventData as PointerEventData;
@@ -34,14 +32,14 @@ public class JoystickMove : MonoBehaviour
         joystickVec = (dragPos - joystickTouchPos).normalized;
 
         float joystickDist = Vector2.Distance(dragPos, joystickTouchPos);
-
+        Vector2 transform = cam.ScreenToWorldPoint(joystickTouchPos);
         if (joystickDist < joystickRadius)
         {
-            joystick.transform.position = joystickTouchPos + joystickVec * joystickDist;
+            joystick.transform.position = transform + joystickVec * joystickDist;
         }
         else
         {
-            joystick.transform.position = joystickTouchPos + joystickVec * joystickRadius;
+            joystick.transform.position = transform + joystickVec * joystickRadius;
         }
     }
 
